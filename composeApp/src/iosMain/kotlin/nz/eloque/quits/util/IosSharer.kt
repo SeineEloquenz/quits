5a46d1a1588646be0d1fabaa9524e7b6dad9c237
Alexander Linder <seine@eloque.nz>
2026-07-27 19:34:32 +0200

    Migrate ios from deprecated apis

--- composeApp/src/iosMain/kotlin/nz/eloque/quits/util/IosSharer.kt
+++ composeApp/src/iosMain/kotlin/nz/eloque/quits/util/IosSharer.kt
@@ -2,21 +2,31 @@ package nz.eloque.quits.util
 
 import platform.UIKit.UIActivityViewController
 import platform.UIKit.UIApplication
+import platform.UIKit.UIViewController
+import platform.UIKit.UIWindowScene
 
 /** Presents a `UIActivityViewController` from the top-most view controller. */
 class IosSharer : Sharer {
     override fun share(text: String) {
+        var top = keyRootViewController() ?: return
+        while (top.presentedViewController != null) {
+            top = top.presentedViewController!!
+        }
         val activity =
             UIActivityViewController(
                 activityItems = listOf(text),
                 applicationActivities = null,
             )
-        var top = UIApplication.sharedApplication.keyWindow?.rootViewController ?: return
-        while (top.presentedViewController != null) {
-            top = top.presentedViewController!!
-        }
         // Anchor the popover (required on iPad) so presentation never crashes.
         activity.popoverPresentationController?.sourceView = top.view
         top.presentViewController(activity, animated = true, completion = null)
     }
+}
+
+/** The active window's root controller, via the foreground scene (UIApplication.keyWindow is gone in recent SDKs). */
+private fun keyRootViewController(): UIViewController? {
+    val scene =
+        UIApplication.sharedApplication.connectedScenes
+            .firstOrNull { it is UIWindowScene } as? UIWindowScene
+    return scene?.keyWindow?.rootViewController
 }
